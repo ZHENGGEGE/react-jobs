99d4fea3d6e3411954101105dc6aa69b32f72deb
ZHENGGEGE <593012736@qq.com>
2017-11-21 00:23:19 +0800

    头像选择

--- src/component/avatar-selector/avatar-selector.js
+++ src/component/avatar-selector/avatar-selector.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Grid } from 'antd-mobile'
 
 
 class AvatarSelector extends React.Component{
     render(){
+        const avatarList = 'boy,bull,chick,crab,girl,hedgehog,hippopotamus,koala,lemur,man,pig,tiger,whale,woman,zebra'
+                            .split(',')
+                            .map(v => ({
+                                icon : require(`../img/${v}.png`),
+                                text : v
+                            }))
         return(
             <div>
+                <Grid data={avatarList}/>
                头像选择
             </div>
         )
