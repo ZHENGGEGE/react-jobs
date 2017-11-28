3c766057220f0ed27256a61f91f9438c6153e000
ZHENGGEGE <593012736@qq.com>
2017-11-28 18:45:21 +0800

    tab

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -1,9 +1,68 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { NavBar, Icon } from 'antd-mobile'
+import NavLinkBar from '../navlink/navlink'
 
-class DashBoard extends React.Component{
+function Boss(){
+    return <div>boss</div>
+}
+
+function Genius(){
+    return <div>Genius</div>
+}
+function Msg(){
+    return <h2>Msg</h2>
+}
+
+function User(){
+    return <h2>user</h2>
+}
+
+@connect(
+    state => state
+)
+
+class DashBoard extends  React.Component{  
     render(){
+        const user = this.props.user
+        const {pathname} = this.props.location
+        const navList = [
+            {
+                path : '/boss',
+                text : '牛人',
+                icon : 'boss',
+                title : '牛人列表',
+                component : Genius,
+                hide : user.type =='genius'
+            },
+            {
+                path : '/genius',
+                text : 'Boss',
+                icon : 'boss',
+                title : 'BOSS列表',
+                component : Boss,
+                hide : user.type =='boss'
+            },
+            {
+                path : '/msg',
+                text : '消息',
+                icon : 'msg',
+                title : '消息列表',
+                component : Msg
+            },
+            {
+                path : '/me',
+                text : '我',
+                icon : 'user',
+                title : '个人中心',
+                component : User
+            }
+        ]
         return(
-            <div>123</div>
+            <div>
+                <NavBar mode="dark">{navList.find(v => v.path==pathname).title}</NavBar>
+                <NavLinkBar data={navList}></NavLinkBar>
+            </div>  
         )
     }
 }
---
+++ src/component/navlink/navlink.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { TabBar } from 'antd-mobile'
+import { withRouter } from 'react-router-dom'
+
+@withRouter
+class NavLinkBar extends React.Component{
+    static PropTypes = {
+        data : PropTypes.array.isRequired
+    }
+    render(){
+        const {pathname} = this.props.location
+        const navList = this.props.data.filter(v => !v.hide)
+        return(
+            <TabBar>
+                {navList.map(v =>(
+                    <TabBar.Item 
+                        key={v.path}
+                        title={v.text}
+                        icon={{uri : require(`./img/${v.icon}.png`)}}
+                        selectedIcon={{uri : require(`./img/${v.icon}-active.png`)}}
+                        selected={pathname === v.path}
+                        onPress={() => {
+                            this.props.history.push(v.path)
+                        }}>
+                    </TabBar.Item>
+                ))}
+            </TabBar>
+        )
+    }
+}
+
+export default NavLinkBar
