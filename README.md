f61458373fb863a0f60b88d87a9af8d3a2ef13f6
ZHENGGEGE <593012736@qq.com>
2018-04-21 15:57:31 +0800

    feat:登录直接跳路由

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ react 实时聊天app
 技术栈：react，ant-mobile,express
 
 npm start 启动
+nodemon server.js 启动服务
 
+
+http://localhost:3000/login 登录页
 ```
--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -22,40 +22,40 @@ class DashBoard extends  React.Component{
         const user = this.props.user
         const {pathname} = this.props.location
         const navList = [
-            {
-                path : '/boss',
-                text : '牛人',
-                icon : 'boss',
-                title : '牛人列表',
-                component : Boss,
-                hide : user.type ==='genius'
-            },
-            {
-                path : '/genius',
-                text : 'Boss',
-                icon : 'job',
-                title : 'BOSS列表',
-                component : Genius,
-                hide : user.type ==='boss'
-            },
-            {
-                path : '/msg',
-                text : '消息',
-                icon : 'msg',
-                title : '消息列表',
-                component : Msg
-            },
-            {
-                path : '/me',
-                text : '我',
-                icon : 'user',
-                title : '个人中心',
-                component : User
-            }
-        ]
+			{
+				path:'/boss',
+				text:'牛人',
+				icon:'boss',
+				title:'牛人列表',
+				component:Boss,
+				hide:user.type=='genius'
+			},
+			{
+				path:'/genius',
+				text:'boss',
+				icon:'job',
+				title:'BOSS列表',
+				component:Genius,
+				hide:user.type=='boss'
+			},
+			{
+				path:'/msg',
+				text:'消息',
+				icon:'msg',
+				title:'消息列表',
+				component:Msg
+			},
+			{
+				path:'/me',
+				text:'我',
+				icon:'user',
+				title:'个人中心',
+				component:User
+			}
+		]
         return(
             <div>
-                <NavBar mode="dark" className="fixed-header">{navList.find(v => v.path===pathname).title}</NavBar>
+                <NavBar className='fixd-header' mode='dard'>{navList.find(v=>v.path===pathname).title}</NavBar>
                 <div>
                     <Switch>
                         {navList.map(v => (
